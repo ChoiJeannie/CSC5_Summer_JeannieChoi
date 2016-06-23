471a2ecdf690d1b789044c2084d83884785271d5
Jeannie Choi <jeanniechoi@Jeannies-MacBook-Pro.local>
2016-06-23 12:43:21 -0700

    CSC5 HW

---
+++ Homework/Assignment 1/Gaddis_8thEd_Chap2_prob15_Triangle/main.cpp
@@ -0,0 +1,35 @@
+/* 
+ * File:   main.cpp
+ * Author: Jeannie Choi
+ * Created on June 22, 2016, 4:12 PM
+ * Purpose:  Triangle Pattern
+ */
+
+//System Libraries
+#include <iostream>  //Input/Output Library
+using namespace std; //Namespace of the System Libraries
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution Begins Here!
+int main(int argc, char** argv) {
+    //Declare Variables
+    
+    //Input Data
+    
+    //Process the Data
+    
+    //Output the processed Data
+    cout <<"    *"<<endl;
+    cout <<"   ***"<<endl;
+    cout <<"  *****"<<endl;
+    cout <<" *******"<<endl;
+    
+    
+    //Exit Stage Right!
+    return 0;
+}
